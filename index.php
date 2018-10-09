c6ca74fed8fd25507bec3125e616a71ff525b828
Jindrich Bobek <jindra74393@windowslive.com>
2018-10-09 19:43:57 +0200

    ! Opravena vznikla chyba.

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
             </div>
             <div class="form-group">
                 <label for="username">Uživatelské jméno</label>
-                <input type="text" class="form-control" name="username" id=""
+                <input type="text" class="form-control" name="username" id="username"
                        value="User" autocomplete="username">
             </div>
             <div class="form-group">
